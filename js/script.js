e396ce17c346c1c37e58948054fa3b01b159d18e
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2023-02-12 19:52:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,187 @@
+/*Index Page */
+
+
+/**Portfolio */
+ScrollReveal().reveal('.anim-port', {delay:350});
+ScrollReveal().reveal('.anim-port2', {delay:450});
+
+
+/*About*/
+ScrollReveal().reveal('.anim1', {delay:250});
+ScrollReveal().reveal('.anim2', {delay:500});
+
+
+
+
+/*Services*/ 
+ScrollReveal().reveal('.anim3', {delay:250});
+ScrollReveal().reveal('.anim4', {delay:500});
+ScrollReveal().reveal('.anim5', {delay:750});
+/*Contact*/
+/*ScrollReveal().reveal('.anim6', {delay:250});
+ScrollReveal().reveal('.anim7', {delay:500});*/
+
+/*Social*/ 
+ScrollReveal().reveal('.anim8', {delay:250});
+ScrollReveal().reveal('.anim9', {delay:500});
+ScrollReveal().reveal('.anim10', {delay:750});
+ScrollReveal().reveal('.anim11', {delay:1000});
+
+
+/*Copyright*/
+ScrollReveal().reveal('.anim12', {delay:1250});
+
+
+
+
+/*Portfolio - About - Services - Contact - Footer  */
+window.sr=ScrollReveal();
+
+sr.reveal('.anim1',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:400 
+
+})
+
+sr.reveal('.anim-left',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:400
+})
+sr.reveal('.anim6',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:400
+})
+sr.reveal('.anim7',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:400
+})
+
+
+sr.reveal('.ani-h4',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:300
+})
+sr.reveal('.ani-h2',{
+    origin: 'right',
+    duration:1000,
+    distance: '25rem',
+    delay:350
+})
+sr.reveal('.ani-h3',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:450
+})
+
+sr.reveal('.ani-form',{
+    origin: 'bottom',
+    duration:1000,
+    distance: '25rem',
+    delay:400
+})
+
+
+
+
+
+
+
+
+/**About Page */
+/*Timeline Item */
+window.sr=ScrollReveal();
+sr.reveal('.anim-ab',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:230
+})
+sr.reveal('.anim-ab2',{
+    origin: 'right',
+    duration:1000,
+    distance: '25rem',
+    delay:330
+})
+sr.reveal('.anim-ab3',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:430
+})
+
+/*footer */
+
+
+/** product page*/
+
+/**Gallery */
+window.sr=ScrollReveal();
+sr.reveal('.ani-gal',{
+    origin: 'top',
+    duration:1000,
+    distance: '25rem',
+    delay:230
+})
+
+
+
+
+
+
+
+
+
+
+/**Blog Page */
+
+sr.reveal('.ani-blog',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:370})
+
+
+
+
+
+
+
+
+
+/**Contact Page */
+
+/**form-img */
+sr.reveal('.ani-con1',{
+    origin: 'left',
+    duration:1000,
+    distance: '25rem',
+    delay:230
+})
+sr.reveal('.ani-con2',{
+    origin: 'right',
+    duration:1000,
+    distance: '25rem',
+    delay:330
+})
+
+
+
+
+
+
+
+
+
+
+
